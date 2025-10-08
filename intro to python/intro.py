8761acc60120d071e723f8e1649112d3b3407641
minsatheunicorn <minsajawed@homail.com>
2025-10-08 20:39:55 +0100

    hello

---
+++ intro to python/intro.py
@@ -0,0 +1,5 @@
+print("hello people")
+print("DEMON SLAYER is SOO SLAY")
+print("IT IS DA BEST THING IN DA WORD")
+print("NOTHING CAN replace DEMON SLAYER")#printing a statement
+print(16)#printing a nuber
